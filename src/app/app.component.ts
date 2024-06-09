3cdf8bcc3c42ca780efbef330583be4ee18e7c2d
lmilunovic <milunovic.ladislav@gmail.com>
2024-06-09 12:12:22 +0200

    Introduce "task" component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,23 +1,27 @@
 import { Component } from '@angular/core';
-import { NgFor } from '@angular/common';
-import { NgIf } from '@angular/common';
+
 
 import { HeaderComponent } from './header/header.component';
 import { UserComponent } from './user/user.component';
 import { TasksComponent } from './tasks/tasks.component';
 import { DUMMY_USERS } from './dummy-users';
-import { NG_ASYNC_VALIDATORS } from '@angular/forms';
+
+interface User {
+  id: string;
+  name: string;
+  avatar: string;
+}
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [HeaderComponent, UserComponent, TasksComponent, NgFor, NgIf],
+  imports: [HeaderComponent, UserComponent, TasksComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
 export class AppComponent {
   users = DUMMY_USERS;
-  selectedUser?: string;
+  selectedUser?: User;
 
   findUserById = (id: string) => {
     return DUMMY_USERS.find((e) => {
@@ -26,7 +30,7 @@ export class AppComponent {
   };
 
   onSelectUser(id: string) {
-    this.selectedUser = this.findUserById(id)?.name;
+    this.selectedUser = this.findUserById(id);
     console.log(id);
   }
 }
