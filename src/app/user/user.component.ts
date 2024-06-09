7a3fbe2bc93f0af78db1fe38ed46716daa084a36
lmilunovic <milunovic.ladislav@gmail.com>
2024-06-09 08:19:56 +0200

    Add "required" option for UserComponent input parameter

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input,  } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-user',
@@ -8,15 +8,12 @@ import { Component, Input,  } from '@angular/core';
   styleUrl: './user.component.css',
 })
 export class UserComponent {
-
-  @Input() avatar!: string;
-  @Input() name!: string;
+  @Input({ required: true }) avatar!: string;
+  @Input({ required: true }) name!: string;
 
   get imagePath() {
     return 'assets/users/' + this.avatar;
   }
 
-  onSelectUser() {
-
-  }
+  onSelectUser() {}
 }
