8d824cd86a4ae5db44e1a509ab505ca12f67ce69
lmilunovic <milunovic.ladislav@gmail.com>
2024-06-09 11:07:40 +0200

    Use tripple ===

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 
-import {HeaderComponent} from './header/header.component';
+import { HeaderComponent } from './header/header.component';
 import { UserComponent } from './user/user.component';
 import { TasksComponent } from './tasks/tasks.component';
 import { DUMMY_USERS } from './dummy-users';
@@ -13,19 +13,17 @@ import { DUMMY_USERS } from './dummy-users';
   styleUrl: './app.component.css',
 })
 export class AppComponent {
-
   users = DUMMY_USERS;
-  selectedUser = "";
+  selectedUser = '';
 
   findUserById = (id: string) => {
-    return DUMMY_USERS.find( (e) => {
-      return e.id == id;
+    return DUMMY_USERS.find((e) => {
+      return e.id === id;
     })!;
-  }
+  };
 
-  onSelectUser(id: string){
+  onSelectUser(id: string) {
     this.selectedUser = this.findUserById(id).name;
     console.log(id);
   }
-
 }
