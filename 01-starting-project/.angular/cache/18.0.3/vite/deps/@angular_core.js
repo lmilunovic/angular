90c0e168af95697bd5f5dbd1d835d73136d98f24
lmilunovic <milunovic.ladislav@gmail.com>
2024-06-30 18:09:29 +0200

    Add new value to gitignore and delete .angular

--- 01-starting-project/.angular/cache/18.0.3/vite/deps/@angular_core.js
+++ 01-starting-project/.angular/cache/18.0.3/vite/deps/@angular_core.js
@@ -457,7 +457,7 @@ import {
   ɵɵvalidateIframeAttribute,
   ɵɵviewQuery,
   ɵɵviewQuerySignal
-} from "./chunk-MS343FM7.js";
+} from "./chunk-5V4RAZ2U.js";
 export {
   ANIMATION_MODULE_TYPE,
   APP_BOOTSTRAP_LISTENER,
